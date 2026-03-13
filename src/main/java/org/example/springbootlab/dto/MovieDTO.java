ae27522a784e15c406ab745f154f91a99e5a3018
Gabriela Aguirre <gvaguirres@gmail.com>
2026-03-13 16:26:08 +0100

    save everything before moving to main

---
+++ src/main/java/org/example/springbootlab/dto/MovieDTO.java
@@ -0,0 +1,54 @@
+package org.example.springbootlab.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class MovieDTO {
+
+    @NotBlank(message = "Title is mandatory")
+    private String title;
+
+    private String description;
+    private String releaseYear;
+    private String director;
+    private int durationMinutes;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
+
+    public void setDurationMinutes(int durationMinutes) {
+        this.durationMinutes = durationMinutes;
+    }
+}
---
+++ src/main/java/org/example/springbootlab/mapper/MovieMapper.java
@@ -0,0 +1,12 @@
+package org.example.springbootlab.mapper;
+
+import org.example.springbootlab.entity.Movie;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface MovieMapper {
+
+    MovieDTO todto = (Movie entity);
+    Movie toEntity = (MovieDTO dto)
+
+}
