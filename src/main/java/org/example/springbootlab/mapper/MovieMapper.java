597fae1c5e382448c053c1d73557ac717b7d63de
Gabriela Aguirre <gvaguirres@gmail.com>
2026-03-13 21:42:04 +0100

    Create movie mapper

--- src/main/java/org/example/springbootlab/mapper/MovieMapper.java
+++ src/main/java/org/example/springbootlab/mapper/MovieMapper.java
@@ -1,12 +1,24 @@
 package org.example.springbootlab.mapper;
 
+import org.example.springbootlab.dto.CreateMovieDTO;
+import org.example.springbootlab.dto.MovieDTO;
+import org.example.springbootlab.dto.UpdateMovieDTO;
 import org.example.springbootlab.entity.Movie;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper
 public interface MovieMapper {
 
-    MovieDTO todto = (Movie entity);
-    Movie toEntity = (MovieDTO dto)
+    //Entity to MovieDTO
+    MovieDTO toDto(Movie movie);
+
+    //Create from Dto to Entity
+    Movie toEntity(CreateMovieDTO createDto);
+
+    //Update an existent Entity
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDto(UpdateMovieDTO updateDto, @MappingTarget Movie movie );
 
 }
